86cad3917070328b04c79144672afd96a6629f59
idf3d <df@dflab.net>
2013-01-24 16:57:29 +0100

    Страничка подтверждения/удаления сообщений. Улучшение #7

--- sources/pg/approval.php
+++ sources/pg/approval.php
@@ -7,6 +7,32 @@ if (!defined('vFIDO_RUN')) {
     echo 'access violation at address FAFAFED';// sorry ;)
     exit();
 }
+
+$hash='';
+$result='';
+if (isset($_GET['approve']))
+{
+    $hash=$_GET['approve'];
+    
+    if (areasApproveMessage($hash,true))
+            $result = 'Подтверждение: успешно.';
+    else
+            $result = 'Подтверждение: не удалось.';
+    
+} else if (isset($_GET['decline']))
+{
+    $hash=$_GET['decline'];
+
+    if (areasApproveMessage($hash,false))
+            $result = 'Удаление: успешно.';
+    else
+            $result = 'Удаление: не удалось.';
+
+    
+} else {
+    $result='Неизвестная операция.';
+}
+
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
@@ -16,6 +42,6 @@ if (!defined('vFIDO_RUN')) {
 <title></title>
 </head>
 <body>
-    
+    <br /><br /><center><h2><?php echo $result; ?></h2></center>
 </body>
 </html>
